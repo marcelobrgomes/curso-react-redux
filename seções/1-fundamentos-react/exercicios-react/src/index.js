08456cca3219f5aa203e495b064619d1ed19dd8b
marcelobrgomes <marcelobrgomes@gmail.com>
2020-04-06 18:21:21 -0300

    exportando múltiplos componentes

--- seções/1-fundamentos-react/exercicios-react/src/index.js
+++ seções/1-fundamentos-react/exercicios-react/src/index.js
@@ -1,11 +1,16 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import PrimeiroComponente from './componentes/PrimeiroComponente'
+import {CompA, CompB as B} from './componentes/DoisComponentes'
+
 
 const elemento = document.getElementById("root")
 
 ReactDOM.render(
     <div>
         <PrimeiroComponente valor="Testando..."/>
+        <CompA valor="Olá"/>
+        <B valor="Fulano"/>
+
     </div>
 , elemento)
